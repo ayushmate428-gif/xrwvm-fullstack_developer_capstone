d928e6b9f2684a92736a79b6997ab716bf4272a2
ayushmate428-gif <ayushmate428@gmail.com>
2026-09-05 17:57:43 +0530

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,18 +1,14 @@
-# Uncomment the imports before you add the code
-# from django.urls import path
-from django.conf.urls.static import static
-from django.conf import settings
-# from . import views
+from django.urls import path
+from . import views
 
 app_name = 'djangoapp'
+
 urlpatterns = [
-    # # path for registration
+    path('login', views.login_user, name='login'),
+    path('logout', views.logout, name='logout'),
+    path('register', views.registration, name='register'),
 
-    # path for login
-    # path(route='login', view=views.login_user, name='login'),
-
-    # path for dealer reviews view
-
-    # path for add a review view
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('get_dealers', views.get_dealers, name='get_dealers'),
+    path('get_dealer/<int:dealer_id>', views.get_dealer, name='get_dealer'),
+    path('get_dealer_reviews/<int:dealer_id>', views.get_dealer_reviews, name='get_dealer_reviews'),
+]
