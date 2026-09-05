0d0e5367bbab9fe9b05a6d659c61cdbc4c1dc64f
ayushmate428-gif <ayushmate428@gmail.com>
2026-09-05 18:10:13 +0530

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,22 +1,42 @@
-# Uncomment the imports below before you add the function code
-# import requests
+import requests
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 backend_url = os.getenv(
-    'backend_url', default="http://localhost:3030")
+"backend_url",
+default="http://localhost:3030"
+)
+
 sentiment_analyzer_url = os.getenv(
-    'sentiment_analyzer_url',
-    default="http://localhost:5050/")
+"sentiment_analyzer_url",
+default="http://localhost:5050/"
+)
 
-# def get_request(endpoint, **kwargs):
-# Add code for get requests to back end
+def get_request(endpoint, **kwargs):
+params = ""
+for key, value in kwargs.items():
+params += f"{key}={value}&"
 
-# def analyze_review_sentiments(text):
-# request_url = sentiment_analyzer_url+"analyze/"+text
-# Add code for retrieving sentiments
+```
+request_url = backend_url + endpoint
+if params:
+    request_url += "?" + params
 
-# def post_review(data_dict):
-# Add code for posting review
+response = requests.get(request_url)
+response.raise_for_status()
+return response.json()
+```
+
+def analyze_review_sentiments(text):
+request_url = sentiment_analyzer_url + "analyze/" + text
+response = requests.get(request_url)
+response.raise_for_status()
+return response.json()
+
+def post_review(data_dict):
+request_url = backend_url + "/insert_review"
+response = requests.post(request_url, json=data_dict)
+response.raise_for_status()
+return response.json()
