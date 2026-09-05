971493cc89f0f8deda3e969ab8d7712a314c229e
ayushmate428-gif <ayushmate428@gmail.com>
2026-09-05 17:58:33 +0530

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,16 +1,15 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
-from django.conf.urls.static import static
-from django.conf import settings
+from django.urls import path
+from . import views
 
-from djangoapp import views
+app_name = 'djangoapp'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('djangoapp/', include('djangoapp.urls')),
+path('login', views.login_user, name='login'),
+path('logout', views.logout_request, name='logout'),
+path('register', views.registration, name='register'),
 
-    path('fetchReviews/dealer/<int:dealer_id>', views.get_dealer_reviews),
+```
+path('get_dealer_reviews/<int:dealer_id>', views.get_dealer_reviews, name='get_dealer_reviews'),
+```
 
-    path('', TemplateView.as_view(template_name="Home.html")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
